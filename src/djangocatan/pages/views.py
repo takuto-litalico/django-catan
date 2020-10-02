ce8619a062c3818224fc5f1bb0acfe096b091b41
takuto-litalico <60427297+takuto-litalico@users.noreply.github.com>
2020-10-02 17:45:21 +0900

    Initial commit

---
+++ src/djangocatan/pages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    return render(request, "index.html", {})
+
+def addgame_view(request, *args, **kwargs):
+    return render(request, "addgame.html", {})
